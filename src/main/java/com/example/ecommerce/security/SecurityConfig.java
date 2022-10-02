bc6f0ecced9c7bae4380963522ad4bde4f9606f9
Jakub Czarnowski <58054635+jakubczarnowski@users.noreply.github.com>
2022-10-02 22:39:29 +0200

    fix

--- src/main/java/com/example/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/example/ecommerce/security/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    protected CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "UPDATE"));
